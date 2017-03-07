d51a74ba0852d3c823e4f0502f1b69850b0e4e0b
ashmna <mnatsakanyan.ashot@gmail.com>
2017-03-08 00:57:27 +0400

    Improved and fixed

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# mongoDB
+kubectl apply -f ./mongo/deployment.yaml
+kubectl apply -f ./mongo/service.yaml
+
+# spiderslab-site
+kubectl apply -f ./spiderslab-site/deployment.yaml
+kubectl apply -f ./spiderslab-site/service.yaml
+
+# pick-front
+kubectl apply -f ./pick-front/deployment.yaml
+kubectl apply -f ./pick-front/service.yaml
+
+# pick
+kubectl apply -f ./pick/deployment.yaml
+kubectl apply -f ./pick/service.yaml
+
+# kube-lego
+kubectl apply -f ./kube-lego/namespace.yaml
+kubectl apply -f ./kube-lego/configmap.yaml
+kubectl apply -f ./kube-lego/deployment.yaml
+
+# ingress
+kubectl apply -f ./ingress/ingress.yaml
